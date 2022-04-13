aad92b7cc7c051fbf1c82f2a2cf33e27c8a0c6f5
Wallace Costa <wallacedacosta@gmail.com>
2022-04-13 16:03:48 -0300

    implementando forms

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -1,5 +1,6 @@
 import { CartService } from './../cart.service';
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-cart',
@@ -9,12 +10,23 @@ import { Component, OnInit } from '@angular/core';
 export class CartComponent implements OnInit {
 
   items = this.cartService.getItems();
+  carrinhoForm = this.formBuilder.group({
+    nome: '',
+    endereco: ''
+  });
 
   constructor(
-    private cartService: CartService
+    private cartService: CartService,
+    private formBuilder: FormBuilder
   ) { }
 
   ngOnInit(): void {
+  }
+
+  onSubmit(): void {
+    this.items = this.cartService.clearCart();
+    console.warn('Seu pedido foi enviado', this.carrinhoForm.value);
+    this.carrinhoForm.reset();
   }
 
 }
